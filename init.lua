5386b1a8644c6402a7e82a7fb03ed6bbd25fb828
Matthew Clark <matthew.clark@taillight.com>
2023-01-17 16:09:48 -0600

    adds wilder plugin

--- init.lua
+++ init.lua
@@ -148,6 +148,38 @@ packer.startup(function(use)
         'nvim-treesitter/nvim-treesitter',
         run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
     }
+
+    use {
+        'gelguy/wilder.nvim',
+        config = function()
+            local wilder = require('wilder')
+
+            wilder.setup({
+                modes = {':'},
+            })
+
+            wilder.set_option('renderer', wilder.popupmenu_renderer(
+                wilder.popupmenu_border_theme({
+                    border = 'rounded',
+                    highlights = {
+                        border = 'Normal',
+                    },
+                    left = {' ', wilder.popupmenu_devicons()},
+                    right = {' ', wilder.popupmenu_scrollbar()},
+                })
+            ))
+
+            wilder.set_option('pipeline', {
+                wilder.branch(
+                    wilder.cmdline_pipeline({
+                        language = 'python',
+                        fuzzy = 2,
+                    })
+                ),
+            })
+        end,
+        run = ':UpdateRemotePlugins',
+    }
 end)
 -- }}}
 
