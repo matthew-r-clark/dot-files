a6de222bfd3f85d93075205e9462ec036fab4676
Matthew Clark <clark.matthewr@gmail.com>
2023-02-07 08:03:18 -0600

    removes log statement

--- init.lua
+++ init.lua
@@ -81,7 +81,6 @@ opt.wrap = false
 local packer = require('packer')
 
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-print(install_path)
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
   exec([[packadd packer.nvim]], false)
