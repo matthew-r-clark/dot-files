91a7e20dcf803bd9be3116a7002352aae7b0c461
Matthew Clark <clark.matthewr@gmail.com>
2023-02-15 12:01:43 -0600

    removes duplicate plugin usage of nvim-lspconfig

--- init.lua
+++ init.lua
@@ -104,15 +104,15 @@ packer.init({
 packer.startup(function(use)
     use 'wbthomason/packer.nvim'
 
-    use { -- Configurations for Nvim LSP
-        'neovim/nvim-lspconfig',
-        config = function()
-            require('lspconfig')['quick_lint_js'].setup({
-                    on_attach = on_attach,
-            })
-        end
-    }
-
+    -- use { -- Configurations for Nvim LSP
+    --     'neovim/nvim-lspconfig',
+    --     config = function()
+    --         require('lspconfig')['quick_lint_js'].setup({
+    --                 on_attach = on_attach,
+    --         })
+    --     end
+    -- }
+    --
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v1.x',
