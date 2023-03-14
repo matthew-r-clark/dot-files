dd098295e60db15942b24af05df1371e93ed432b
Matthew Clark <clark.matthewr@gmail.com>
2023-03-14 13:46:41 -0500

    adds folding around comment

--- init.lua
+++ init.lua
@@ -69,7 +69,7 @@ opt.formatoptions = 'crqnj'
     n -> recognized numbered lists
     j -> auto-remove comments when joining lines
 ]]
---- }}}
+-- }}}
 -- opt.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
 opt.ignorecase = true
 opt.lazyredraw = true
