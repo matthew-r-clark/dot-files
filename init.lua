536a2d7845c1f4e768ae05daa95f47546d1ff110
Matthew Clark <clark.matthewr@gmail.com>
2023-04-15 13:32:19 -0500

    removes indentLine and tabnine

--- init.lua
+++ init.lua
@@ -326,25 +326,6 @@ packer.startup(function(use)
     use {
         'mattn/calendar-vim',
     }
-
-    use {
-        'Yggdroot/indentLine'
-    }
-
-    use { -- AI autocomplete (similar to github copilot)
-        'codota/tabnine-nvim',
-        run = './dl_binaries.sh',
-        config = function ()
-            require('tabnine').setup({
-                disable_auto_comment = true,
-                accept_keymap = '<Tab>',
-                dismiss_keymap = '<C-]',
-                debounce_ms = 800,
-                suggestion_color = { gui = '#808080', cterm = 244 },
-                exclude_filetypes = { 'TelescopePrompt' },
-            })
-        end
-    }
 end)
 
 exec([[
