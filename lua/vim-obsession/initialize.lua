39ae7a9829c0070be6249947e16f70eeee710505
Matthew Clark <clark.matthewr@gmail.com>
2023-04-15 23:07:14 -0500

    move plugin installation and config into separate module, refactor vim session initialization

--- lua/vim-obsession/initialize.lua
+++ lua/vim-obsession/initialize.lua
@@ -1,14 +1,12 @@
-local cwd = vim.fn.getcwd()
-local sessionFilePath = cwd..'/Session.vim'
-
 local function session_exists()
+    local sessionFilePath = string.format("%s/Session.vim", vim.fn.getcwd())
     local f=io.open(sessionFilePath, 'r')
     if f~=nil then io.close(f) return true else return false end
 end
 
 local function initialize()
     if session_exists() then
-        vim.cmd('source Session.vim')
+        vim.cmd(':source Session.vim')
     else
         vim.cmd(':Obsession')
     end
