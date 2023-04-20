0ae6be6e1218cf67215173575734ffb37b032415
Matthew Clark <clark.matthewr@gmail.com>
2023-04-19 19:53:02 -0500

    refactor conditions in session_exists to be multiline for readability

--- lua/vim-obsession/initialize.lua
+++ lua/vim-obsession/initialize.lua
@@ -1,7 +1,13 @@
 local function session_exists()
     local sessionFilePath = string.format("%s/Session.vim", vim.fn.getcwd())
     local f=io.open(sessionFilePath, 'r')
-    if f~=nil then io.close(f) return true else return false end
+
+    if f~=nil then
+        io.close(f)
+        return true
+    else
+        return false
+    end
 end
 
 local function initialize()
