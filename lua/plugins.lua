37453fbd3ffcdf4e1bc666582d9d5bcfd3bd1ff9
Matthew Clark <clark.matthewr@gmail.com>
2023-04-30 13:13:19 -0500

    comment out autocmd to open nvim-tree on VimEnter, may find a way to more intelligently use later

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -376,10 +376,12 @@ require('nvim-tree').setup({
         },
     },
 })
-local function open_nvim_tree()
-    require('nvim-tree.api').tree.open()
-end
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+-- local function open_nvim_tree()
+--     require('nvim-tree.api').tree.open()
+-- end
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
 require('nvim-web-devicons').setup({ default = true })
 
 require('nightfox').setup({
