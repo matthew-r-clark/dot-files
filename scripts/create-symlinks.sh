12d2828e19fe671d6ddfd30fdc339b24ddba8bc9
Matthew Clark <matthew.clark@taillight.com>
2023-12-12 12:25:09 -0600

    fix create-symlinks.sh script to include explicit paths

--- scripts/create-symlinks.sh
+++ scripts/create-symlinks.sh
@@ -1,10 +1,12 @@
 #!/bin/zsh
 
-ln -sfF ${DOTFILE_DIR}/init.lua ~/.config/nvim/init.lua
-ln -sfF ${DOTFILE_DIR}/lua ~/.config/nvim
-ln -sfF ${DOTFILE_DIR}/after ~/.config/nvim
-ln -sfF ${DOTFILE_DIR}/snippets ~/.config/nvim
-ln -sfF ${DOTFILE_DIR}/.gitconfig ~/.gitconfig
-ln -sfF ${DOTFILE_DIR}/.gitignore ~/.gitignore
-ln -sfF ${DOTFILE_DIR}/.tmux.conf ~/.tmux.conf
-ln -sfF ${DOTFILE_DIR}/.zshrc ~/.zshrc
+DOTFILE_DIR="$HOME/dot-files"
+
+ln -sfF ${DOTFILE_DIR}/init.lua ${HOME}/.config/nvim/init.lua
+ln -sfF ${DOTFILE_DIR}/lua ${HOME}/.config/nvim/lua
+ln -sfF ${DOTFILE_DIR}/after ${HOME}/.config/nvim/after
+ln -sfF ${DOTFILE_DIR}/snippets ${HOME}/.config/nvim/snippets
+ln -sfF ${DOTFILE_DIR}/.gitconfig ${HOME}/.gitconfig
+ln -sfF ${DOTFILE_DIR}/.gitignore ${HOME}/.gitignore
+ln -sfF ${DOTFILE_DIR}/.tmux.conf ${HOME}/.tmux.conf
+ln -sfF ${DOTFILE_DIR}/.zshrc ${HOME}/.zshrc
