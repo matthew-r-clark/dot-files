e53b6d5453bee9fac1643b210a0f1725f210cb96
Matthew Clark <matthew.clark@taillight.com>
2023-12-16 22:08:13 -0600

    use opts instead of config for neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
       "MunifTanjim/nui.nvim",
       "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
-    config = {
+    opts = {
         close_if_last_window = true,
         filesystem = {
             filtered_items = {
