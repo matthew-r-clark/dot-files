4d2f24aab9b5bdf6e08b1034b7caed55c0c38e62
Matthew Clark <matthew.clark@taillight.com>
2023-12-19 16:46:16 -0600

    comment out code that causes conflicts with selecting next/previous option from wilder menu

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -39,8 +39,8 @@ return { -- Autocompletion
                         cmp.select_next_item()
                     elseif luasnip.expand_or_jumpable() then
                         luasnip.expand_or_jump()
-                    elseif has_words_before() then
-                        cmp.complete()
+                    -- elseif has_words_before() then
+                    --     cmp.complete()
                     else
                         fallback()
                     end
@@ -48,8 +48,8 @@ return { -- Autocompletion
                 ['<S-Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
-                    elseif luasnip.jumpable(-1) then
-                        luasnip.jump(-1)
+                    -- elseif luasnip.jumpable(-1) then
+                    --     luasnip.jump(-1)
                     else
                         fallback()
                     end
