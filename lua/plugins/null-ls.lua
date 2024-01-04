9540a7d33077c0e9644b7de26240c017fe89469c
Matthew Clark <matthew.clark@taillight.com>
2024-01-03 23:58:05 -0600

    remove cspell diagnostics

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -13,7 +13,6 @@ return { -- helps with setting up LSP sources
                 null_ls.builtins.code_actions.eslint_d,
                 null_ls.builtins.code_actions.cspell,
                 null_ls.builtins.code_actions.shellcheck,
-                -- null_ls.builtins.diagnostics.cspell,
                 null_ls.builtins.diagnostics.eslint_d,
                 null_ls.builtins.diagnostics.shellcheck,
                 null_ls.builtins.formatting.prettierd,
