145a81352ac36209957ef7584d497d65e00d0600
Matthew Clark <matthew.clark@taillight.com>
2024-01-07 00:10:41 -0600

    refactored nightfox plugin to not need MERGE_TABLES

--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -2,25 +2,22 @@ return { -- vim syntax theme (Nord)
     'EdenEast/nightfox.nvim',
     lazy = false,
     priority = 1000,
-    opts = function(_, default_opts)
+    init = function ()
         vim.cmd(':colorscheme nordfox')
-
-        local custom_opts = {
-            options = {
-                transparent = true,
-                dim_inactive = true,
-                styles = {
-                    keywords = 'bold',
-                },
+    end,
+    opts = {
+        options = {
+            transparent = true,
+            dim_inactive = true,
+            styles = {
+                keywords = 'bold',
             },
-            groups = {
-                all = {
-                    CursorLine = { bg = '#3B4252' },
-                    CursorColumn = { bg = '#3B4252' },
-                },
+        },
+        groups = {
+            all = {
+                CursorLine = { bg = '#3B4252' },
+                CursorColumn = { bg = '#3B4252' },
             },
-        }
-
-        return MERGE_TABLES(default_opts, custom_opts)
-    end
+        },
+    },
 }
