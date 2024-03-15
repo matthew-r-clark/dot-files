07f467f992686ad6b17e35f9996e5a002dacb612
Matthew Clark <matthew.clark@taillight.com>
2024-03-15 00:51:28 -0500

    use different plugins for JSX/Typescript syntax highlighting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,7 +3,8 @@ return {
     { 'christoomey/vim-sort-motion' }, -- sort bindings
     { 'christoomey/vim-tmux-navigator' }, -- smart pane switcher
     { 'mattn/calendar-vim' }, -- calendar for vimwiki
-    { 'maxmellon/vim-jsx-pretty' }, -- React syntax highlighting
+    { 'leafgarland/typescript-vim' }, -- syntax file and other settings for TypeScript
+    { 'peitalin/vim-jsx-typescript' }, -- syntax highlighting and indentation for JSX in Typescript
     { 'mg979/vim-visual-multi' }, -- multi cursor
     { 'sheerun/vim-polyglot' }, -- vim language packs
     { 'tpope/vim-surround' }, -- modify surround chars
