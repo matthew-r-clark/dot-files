4adc9bd42e5dcdc40294c56969f2b43a6ddf02c9
Matthew Clark <matthew.clark@taillight.com>
2024-03-15 00:47:35 -0500

    reenable neotree with different shortcut to use in case telescope-file-browser is insufficient

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,25 +1,27 @@
+local map = vim.keymap.set
+
 return {
---     "nvim-neo-tree/neo-tree.nvim",
---     branch = "v3.x",
---     dependencies = {
---       "nvim-lua/plenary.nvim",
---       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
---       "MunifTanjim/nui.nvim",
---       "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
---     },
---     init = function()
---         map('n', '<leader>o', ':Neotree toggle reveal float<CR>', {})
---     end,
---     opts = {
---         close_if_last_window = true,
---         filesystem = {
---             filtered_items = {
---                 hide_dotfiles = false,
---                 hide_gitignored = false,
---             },
---             follow_current_file = {
---                 enabled = true,
---             },
---         },
---     },
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    init = function()
+        map('n', '<leader>O', ':Neotree toggle reveal float<CR>', {})
+    end,
+    opts = {
+        close_if_last_window = true,
+        filesystem = {
+            filtered_items = {
+                hide_dotfiles = false,
+                hide_gitignored = false,
+            },
+            follow_current_file = {
+                enabled = true,
+            },
+        },
+    },
 }
