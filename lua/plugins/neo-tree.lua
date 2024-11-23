3fa268296925e01aaad9dd43bd50b718c86aba2e
Matthew Clark <matthew.clark@taillight.com>
2024-11-22 23:32:33 -0600

    disable and move autocmd fns into plugin module, use config fn instead of opts table

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,5 +1,22 @@
 local map = vim.keymap.set
 
+local function buf_modified(bufnr)
+    return vim.api.nvim_buf_get_option(bufnr, 'modified')
+end
+
+local function buf_empty(bufnr)
+    local lines = vim.api.nvim_buf_get_lines(bufnr, -2, -1, false)
+    return #lines == 1 and #lines[1] == 0
+end
+
+local function open_neo_tree()
+    local current_buffer = vim.api.nvim_get_current_buf();
+
+    if (not buf_modified(current_buffer) and buf_empty(current_buffer)) then
+        vim.cmd(':Neotree float<CR>')
+    end
+end
+
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -12,16 +29,20 @@ return {
     init = function()
         map('n', '<leader>O', ':Neotree toggle reveal float<CR>', {})
     end,
-    opts = {
-        close_if_last_window = true,
-        filesystem = {
-            filtered_items = {
-                hide_dotfiles = false,
-                hide_gitignored = false,
+    config = function()
+        require('neo-tree').setup({
+            close_if_last_window = true,
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                },
+                follow_current_file = {
+                    enabled = true,
+                },
             },
-            follow_current_file = {
-                enabled = true,
-            },
-        },
-    },
+        })
+
+        -- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_neo_tree })
+    end,
 }
