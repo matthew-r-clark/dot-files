8d0d210072b8be8cb94c71628c32c833b55af42d
Matthew Clark <matthew.clark@taillight.com>
2024-11-27 22:14:55 -0600

    install flash.nvim

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,11 +1,12 @@
-return {
+return { -- code navigation
   'folke/flash.nvim',
   event = 'VeryLazy',
   keys = {
-    { 's', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash' },
-    { 'S', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter() end, desc = 'Flash Treesitter' },
-    { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Remote Flash' },
-    { 'R', mode = { 'o', 'x' }, function() require('flash').treesitter_search() end, desc = 'Treesitter Search' },
-    { '<c-s>', mode = { 'c' }, function() require('flash').toggle() end, desc = 'Toggle Flash Search' },
+    { '<leader>f', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash search' },
+    -- learn how these work before enabling:
+    -- { 'F', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter() end, desc = 'Flash select' },
+    -- { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Remote Flash' },
+    -- { 'R', mode = { 'o', 'x' }, function() require('flash').treesitter_search() end, desc = 'Treesitter Search' },
+    -- { '<c-s>', mode = { 'c' }, function() require('flash').toggle() end, desc = 'Toggle Flash Search' },
   },
 }
