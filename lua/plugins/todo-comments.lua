315a2551ddc1126a690512ac58f3cae53802fc1e
Matthew Clark <matthew.clark@taillight.com>
2024-12-02 10:56:13 -0600

    add keybinding to display todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,5 +1,12 @@
 return {
     'folke/todo-comments.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+        {
+            '<leader>tc',
+            '<cmd>TodoTelescope<cr>',
+            desc = 'view todo comments',
+        },
+    },
     opts = {},
 }
