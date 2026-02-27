3f21b2020592d9b6ff0255c98f9fed9c89cd43fb
Matthew Clark <matthew.clark@taillight.com>
2026-02-26 23:47:59 -0600

    add debugger (old WIP)

---
+++ lua/plugins/debugger.lua
@@ -0,0 +1,211 @@
+return {
+    'mfussenegger/nvim-dap',
+    lazy = true,
+    dependencies = {
+        'rcarriga/nvim-dap-ui',
+
+        -- Required dependency for nvim-dap-ui
+        'nvim-neotest/nvim-nio',
+
+        -- Installs the debug adapters for you
+        'williamboman/mason.nvim',
+        'jay-babu/mason-nvim-dap.nvim',
+
+        -- Add your own debuggers here
+        'leoluz/nvim-dap-go',
+        'mxsdev/nvim-dap-vscode-js',
+        {
+            'microsoft/vscode-js-debug',
+            build = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out',
+        },
+    },
+    keys = {
+        {
+            '<F5>',
+            function()
+                require('dap').continue()
+            end,
+            desc = 'Debug: Start/Continue',
+        },
+        -- {
+        --     '<F1>',
+        --     function()
+        --         require('dap').step_into()
+        --     end,
+        --     desc = 'Debug: Step Into',
+        -- },
+        -- {
+        --     '<F2>',
+        --     function()
+        --         require('dap').step_over()
+        --     end,
+        --     desc = 'Debug: Step Over',
+        -- },
+        -- {
+        --     '<F3>',
+        --     function()
+        --         require('dap').step_out()
+        --     end,
+        --     desc = 'Debug: Step Out',
+        -- },
+        {
+            '<leader>b',
+            function()
+                require('dap').toggle_breakpoint()
+            end,
+            desc = 'Debug: Toggle Breakpoint',
+        },
+        -- {
+        --     '<leader>B',
+        --     function()
+        --         require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+        --     end,
+        --     desc = 'Debug: Set Breakpoint',
+        -- },
+        -- -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+        -- {
+        --     '<F7>',
+        --     function()
+        --         require('dapui').toggle()
+        --     end,
+        --     desc = 'Debug: See last session result.',
+        -- },
+    },
+    config = function()
+        local dap = require('dap')
+        local dapui = require('dapui')
+
+        require('mason-nvim-dap').setup {
+            -- Makes a best effort to setup the various debuggers with
+            -- reasonable debug configurations
+            automatic_installation = true,
+
+            -- You can provide additional configuration to the handlers,
+            -- see mason-nvim-dap README for more information
+            handlers = {},
+
+            -- You'll need to check that you have the required things installed
+            -- online, please don't ask me how to install them :)
+            ensure_installed = {
+                -- Update this to ensure that you have the debuggers for the langs you want
+                'js',
+                'js-debug-adapter',
+            },
+        }
+
+        -- Dap UI setup
+        -- For more information, see |:help nvim-dap-ui|
+        dapui.setup {
+            -- Set icons to characters that are more likely to work in every terminal.
+            --    Feel free to remove or use ones that you like more! :)
+            --    Don't feel like these are good choices.
+            icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+            controls = {
+                icons = {
+                    pause = '⏸',
+                    play = '▶',
+                    step_into = '⏎',
+                    step_over = '⏭',
+                    step_out = '⏮',
+                    step_back = 'b',
+                    run_last = '▶▶',
+                    terminate = '⏹',
+                    disconnect = '⏏',
+                },
+            },
+        }
+
+        -- Change breakpoint icons
+        -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+        -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
+        -- local breakpoint_icons = vim.g.have_nerd_font
+        --     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+        --   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+        -- for type, icon in pairs(breakpoint_icons) do
+        --   local tp = 'Dap' .. type
+        --   local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+        --   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+        -- end
+
+        dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+        dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+        dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+        -- Install golang specific config
+        require('dap-go').setup {
+            delve = {
+                -- On Windows delve must be run attached or it crashes.
+                -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+                detached = vim.fn.has 'win32' == 0,
+            },
+        }
+
+        -- local dap_vscode_js_debug_path = vim.fn.stdpath('data') .. '/lazy/vscode-js-debug'
+        -- local dap_vscode_js_debug_path = vim.fn.stdpath('data') .. '/mason/packages/js-debug-adapter'
+        local dap_vscode_js_debug_path = vim.fn.stdpath('data') .. '/lazy/vscode-js-debug/out/src/vsDebugServer.js'
+        require('dap-vscode-js').setup({
+            -- node_path = 'node', -- Path of node executable. Defaults to $NODE_PATH, and then 'node'
+            -- debugger_path = '(runtimedir)/site/pack/packer/opt/vscode-js-debug', -- Path to vscode-js-debug installation.
+            debugger_path = dap_vscode_js_debug_path,
+            debugger_cmd = { 'js-debug-adapter' }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+            adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+            -- log_file_path = '(stdpath cache)/dap_vscode_js.log' -- Path for file logging
+            -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+            -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+        })
+
+        dap.adapters['pwa-node'] = {
+            type = 'server',
+            host = '::1', -- The host the adapter should connect to
+            port = '${port}', -- Replace with the port you're using (e.g., 8130)
+            executable = {
+                -- command = 'node',
+                -- args = {
+                --     vim.fn.stdpath('data') .. '/lazy/vscode-js-debug/out/src/vsDebugServer.js',
+                -- },
+                command = 'js-debug-adapter',
+                args = {
+                    '${port}',
+                },
+            },
+        }
+
+        for _, language in ipairs({ 'typescript', 'javascript' }) do
+            require('dap').configurations[language] = {
+                -- https://github.com/mxsdev/nvim-dap-vscode-js?tab=readme-ov-file#configurations
+                -- {
+                --     type = 'pwa-node',
+                --     request = 'launch',
+                --     name = 'Launch file',
+                --     program = '${file}',
+                --     cwd = '${workspaceFolder}',
+                -- },
+                {
+                    type = 'pwa-node',
+                    address = 'localhost',
+                    port = 9228,
+                    request = 'attach',
+                    name = 'Docker: attach to node (subscriptions api)',
+                    remoteRoot = '/app/src',
+                    localRoot = '${workspaceRoot}/src',
+                    -- processId = require('dap.utils').pick_process,
+                    -- cwd = '${workspaceFolder}/app',
+                    restart = true,
+                    skipFiles = { '${workspaceFolder}/node_modules/**/*.js' },
+
+                    -- name = 'Attach to node process',
+                    -- type = 'pwa-node',
+                    -- request = 'attach',
+                    -- rootPath = '${workspaceFolder}/src',
+                    -- processId = require('dap.utils').pick_process,
+                    -- sourceMaps = true,
+                    -- resolveSourceMapLocations = { '${workspaceFolder}/**', '!**/node_modules/**' },
+                    -- skipFiles = { '${workspaceFolder}/node_modules/**/*.js' },
+                }
+            }
+        end
+
+        -- require('dap.ext.vscode').json_decode = require'json5'.parse
+        -- require('dap.ext.vscode').load_launchjs()
+    end,
+}
