d1974fa818ac9e94e145ce2797183353fe0670f4
Matthew Clark <matthew.clark@taillight.com>
2026-02-26 23:59:15 -0600

    remove easymotion, replaced by flash (not sure flash is much better?)

--- lua/plugins/easymotion.lua
+++
@@ -1,8 +0,0 @@
-local map = vim.keymap.set
-
-return { -- find letter
-    'easymotion/vim-easymotion',
-    init = function()
-        map('n', '<leader>f', '<Plug>(easymotion-bd-f)', {})
-    end,
-}
