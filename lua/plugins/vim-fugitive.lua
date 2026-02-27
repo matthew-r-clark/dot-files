a98a85c2c860ed8cf51acf606f7154c66b2885b1
Matthew Clark <matthew.clark@taillight.com>
2026-02-26 23:54:54 -0600

    add git merge tool keybindings for vim-fugitive

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -6,5 +6,8 @@ return {
         map('', '<leader>gd', ':Gvdiff<CR>', { desc = 'git diff' })
         map('', '<leader>gb', ':Git blame<CR>', { desc = 'git blame' })
         map('', '<leader>gs', ':Git<CR>', { desc = 'git status'})
+        map('', '<leader>gm', ':Gdiffsplit!<CR>', { desc = 'git merge tool' })
+        map('', '<leader>gl', ':diffget //2<CR>', { desc = 'git merge: take THEIR changes (left)' })
+        map('', '<leader>gr', ':diffget //3<CR>', { desc = 'git merge: take YOUR changes (right)' })
     end
 }
