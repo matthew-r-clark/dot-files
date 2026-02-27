0ff3db5868099fb6f9fd17673c669bf7e094e001
Matthew Clark <matthew.clark@taillight.com>
2026-02-27 00:15:03 -0600

    move lazy-lock.json into version control to allow rolling back plugin updates

--- scripts/create-symlinks.sh
+++ scripts/create-symlinks.sh
@@ -6,6 +6,7 @@ ln -sfF ${DOTFILE_DIR}/init.lua ${HOME}/.config/nvim/init.lua
 ln -sfF ${DOTFILE_DIR}/lua ${HOME}/.config/nvim/lua
 ln -sfF ${DOTFILE_DIR}/after ${HOME}/.config/nvim/after
 ln -sfF ${DOTFILE_DIR}/snippets ${HOME}/.config/nvim/snippets
+ln -sfF ${DOTFILE_DIR}/lua/lazy-lock.json ${HOME}/.config/nvim/lazy-lock.json
 ln -sfF ${DOTFILE_DIR}/.gitconfig ${HOME}/.gitconfig
 ln -sfF ${DOTFILE_DIR}/.gitignore ${HOME}/.gitignore
 ln -sfF ${DOTFILE_DIR}/.tmux.conf ${HOME}/.tmux.conf
