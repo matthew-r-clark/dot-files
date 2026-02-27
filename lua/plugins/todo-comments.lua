c4012e80b1386e730ff407d9d605e51d209bcbeb
Matthew Clark <matthew.clark@taillight.com>
2026-02-26 23:54:15 -0600

    add comment with available keywords for todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,3 +1,12 @@
+-- Available keywords:
+--     FIX
+--     TODO
+--     HACK
+--     WARN
+--     PERF
+--     NOTE
+--     TEST
+
 return {
     'folke/todo-comments.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
