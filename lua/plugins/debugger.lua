a226daff131f640da98cf31b8a279f030fab2439
Matthew Clark <matthew.clark@taillight.com>
2026-02-27 08:58:36 -0600

    fix debugger for subscriptionsapi

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -13,11 +13,6 @@ return {
 
         -- Add your own debuggers here
         'leoluz/nvim-dap-go',
-        'mxsdev/nvim-dap-vscode-js',
-        {
-            'microsoft/vscode-js-debug',
-            build = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out',
-        },
     },
     keys = {
         {
@@ -140,20 +135,6 @@ return {
             },
         }
 
-        -- local dap_vscode_js_debug_path = vim.fn.stdpath('data') .. '/lazy/vscode-js-debug'
-        -- local dap_vscode_js_debug_path = vim.fn.stdpath('data') .. '/mason/packages/js-debug-adapter'
-        local dap_vscode_js_debug_path = vim.fn.stdpath('data') .. '/lazy/vscode-js-debug/out/src/vsDebugServer.js'
-        require('dap-vscode-js').setup({
-            -- node_path = 'node', -- Path of node executable. Defaults to $NODE_PATH, and then 'node'
-            -- debugger_path = '(runtimedir)/site/pack/packer/opt/vscode-js-debug', -- Path to vscode-js-debug installation.
-            debugger_path = dap_vscode_js_debug_path,
-            debugger_cmd = { 'js-debug-adapter' }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-            adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
-            -- log_file_path = '(stdpath cache)/dap_vscode_js.log' -- Path for file logging
-            -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-            -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-        })
-
         dap.adapters['pwa-node'] = {
             type = 'server',
             host = '::1', -- The host the adapter should connect to
@@ -187,7 +168,7 @@ return {
                     request = 'attach',
                     name = 'Docker: attach to node (subscriptions api)',
                     remoteRoot = '/app/src',
-                    localRoot = '${workspaceRoot}/src',
+                    localRoot = '${workspaceFolder}/src',
                     -- processId = require('dap.utils').pick_process,
                     -- cwd = '${workspaceFolder}/app',
                     restart = true,
