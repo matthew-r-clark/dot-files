701468e716352607dde3532be6f4cce4b5811b62
Matthew Clark <matthew.clark@taillight.com>
2026-03-04 22:24:01 -0600

    temporarily remove debugger keybindings since they conflict with existing bindings

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -22,27 +22,27 @@ return {
             end,
             desc = 'Debug: Start/Continue',
         },
-        {
-            '<F1>',
-            function()
-                require('dap').step_into()
-            end,
-            desc = 'Debug: Step Into',
-        },
-        {
-            '<F2>',
-            function()
-                require('dap').step_over()
-            end,
-            desc = 'Debug: Step Over',
-        },
-        {
-            '<F3>',
-            function()
-                require('dap').step_out()
-            end,
-            desc = 'Debug: Step Out',
-        },
+        -- {
+        --     '<F1>',
+        --     function()
+        --         require('dap').step_into()
+        --     end,
+        --     desc = 'Debug: Step Into',
+        -- },
+        -- {
+        --     '<F2>',
+        --     function()
+        --         require('dap').step_over()
+        --     end,
+        --     desc = 'Debug: Step Over',
+        -- },
+        -- {
+        --     '<F3>',
+        --     function()
+        --         require('dap').step_out()
+        --     end,
+        --     desc = 'Debug: Step Out',
+        -- },
         {
             '<leader>b',
             function()
