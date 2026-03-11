ecca420248c5ca1029dabcc1536a8c364389a226
Matthew Clark <matthew.clark@taillight.com>
2026-03-10 20:03:56 -0500

    tweak lualine statusline background colors to be 5% lighter than pane background

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,36 +3,53 @@ return { -- vim status line theme
     dependencies = {
         { 'nvim-tree/nvim-web-devicons', lazy = true },
     },
-    opts = {
-        options = {
-            theme = 'nord',
-            section_separators = { left = '', right = '' },
-            component_separators = { left = '', right = '' },
-            disabled_filetypes = { 'NvimTree' },
-            globalstatus = true,
-        },
-        sections = {
-            lualine_b = {},
-            lualine_c = {
-                {
-                    'filename',
-                    color = {
-                        fg = '#D8DEE9',
-                        bg = '#4C566A',
-                    },
-                    path = 3,
-                    shorting_target = 10,
-                },
+    config = function()
+        local theme = require('lualine.themes.nord')
+        -- 5% darker than backgrounds
+        theme.normal.c.bg = '#242935'
+        theme.inactive.c.bg = '#383e4a'
+
+        require('lualine').setup({
+            options = {
+                theme = theme,
+                section_separators = { left = '', right = '' },
+                component_separators = { left = '', right = '' },
+                disabled_filetypes = { 'NvimTree' },
             },
-            lualine_x = {
-                {
-                    'branch',
-                    color = {
-                        fg = '#B48EAD',
+            sections = {
+                lualine_b = {},
+                lualine_c = {
+                    {
+                        'filename',
+                        color = {
+                            fg = '#D8DEE9',
+                        },
+                        path = 3,
+                        shorting_target = 10,
                     },
                 },
+                lualine_x = {
+                    {
+                        'branch',
+                        color = {
+                            fg = '#B48EAD',
+                        },
+                    },
+                },
+                lualine_y = {},
             },
-            lualine_y = {},
-        },
-    },
+            inactive_sections = {
+                lualine_c = {
+                    {
+                        'filename',
+                        color = {
+                            fg = '#D8DEE9',
+                        },
+                        path = 3,
+                        shorting_target = 10,
+                    },
+                },
+            },
+        })
+    end,
 }
