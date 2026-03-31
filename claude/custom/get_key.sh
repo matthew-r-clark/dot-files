b304d95b4a309d62f434872a661a0c302f4727c6
Matthew Clark <matthew.clark@taillight.com>
2026-03-31 11:07:01 -0500

    Update claude get key script to use key stored in 1password.

--- claude/custom/get_key.sh
+++ claude/custom/get_key.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-security find-generic-password -a "$USER" -s claude-api-key -w
-# security find-generic-password -a "$USER" -s open-ai-api-key -w
+get-key ANTHROPIC_API_KEY
+# get-key OPENAI_API_KEY
