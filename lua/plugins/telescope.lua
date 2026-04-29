084ffbefb7a253559d3ff0a212fa32c557395329
Matthew Clark <clark.matthewr@gmail.com>
2026-04-29 14:50:06 -0500

    Bump telescope tagged version.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ local map = vim.api.nvim_set_keymap
 
 return { -- fuzzy finder
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
+    tag = 'v0.2.2',
     dependencies = {
         { 'nvim-lua/plenary.nvim' },
         {
